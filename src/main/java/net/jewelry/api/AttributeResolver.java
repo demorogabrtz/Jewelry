8960e75545c7dbd0de745789f98452f4da5ee9d3
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-04-07 12:39:30 +0200

    Remove dependency on CombatRoll

--- src/main/java/net/jewelry/api/AttributeResolver.java
+++ src/main/java/net/jewelry/api/AttributeResolver.java
@@ -1,6 +1,5 @@
 package net.jewelry.api;
 
-import net.combatroll.api.EntityAttributes_CombatRoll;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.registry.Registries;
@@ -12,6 +11,7 @@ import java.util.HashMap;
 public class AttributeResolver {
     private static final HashMap<Identifier, EntityAttribute> attributes = new HashMap<>();
 
+    @Deprecated
     /**
      * Called upon initialization of this mod.
      */
@@ -21,11 +21,6 @@ public class AttributeResolver {
                 EntityAttributes_ProjectileDamage.attributeId,
                 EntityAttributes_ProjectileDamage.GENERIC_PROJECTILE_DAMAGE
             );
-        }
-        if (FabricLoader.getInstance().isModLoaded("combatroll")) {
-            register(EntityAttributes_CombatRoll.countId, EntityAttributes_CombatRoll.COUNT);
-            register(EntityAttributes_CombatRoll.distanceId, EntityAttributes_CombatRoll.DISTANCE);
-            register(EntityAttributes_CombatRoll.rechargeId, EntityAttributes_CombatRoll.RECHARGE);
         }
     }
 
